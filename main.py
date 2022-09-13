8eec44e527764c21f771d11ccfa39401189a685a
Estevamsl <estevamsouzalaureth@gmail.com>
2022-09-13 15:36:09 -0300

    :tada: initial commit

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def makeCommits (days):
         # days * makeCommits(days - 1)
 
 cont = 0
-maxNumero = 1200
+maxNumero = 1400
 while True:
     # makeCommits(1)
-    makeCommits(1200 - maxNumero)
+    makeCommits(1400 - maxNumero)
     cont += 1
     maxNumero -= 1
-    if cont == 1200:
+    if cont == 1400:
         break
